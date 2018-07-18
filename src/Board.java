a6eb712dbb926a0ea140269511653fa72d60f7bd
MehmetOzturk95 <MehmetOzturk95@gmail.com>
2018-07-17 21:49:26 -0400

    Fixed whitespace

--- src/Board.java
+++ src/Board.java
@@ -3,15 +3,15 @@ public class Board {
     final char EMPTY = ' ';
     final char X = 'X';
     final char O = 'O';
-   
+    
     private char[][] grid;
     private char currentPlayer;
-
+    
     public Board() {
         grid = new char[N][N]; 
         reset();
     }
-
+    
     public void print() {
         for (int r=0; r<N; r++) {
             for (int c=0; c<N; c++) {
@@ -23,7 +23,7 @@ public class Board {
             System.out.println();
         }
     }
-
+    
     public void reset() {
         for (int r=0; r<N; r++) {
             for (int c=0; c<N; c++) {
@@ -32,60 +32,60 @@ public class Board {
         }
         currentPlayer = X; // For now
     }
-
+    
     private boolean didPlayerWin(char p) {
-    	if (grid[0][0] == p && grid[0][1] == p && grid[0][2] == p) return true;
-    	if (grid[1][0] == p && grid[1][1] == p && grid[1][2] == p) return true;
-    	if (grid[2][0] == p && grid[2][1] == p && grid[2][2] == p) return true;
-    	if (grid[0][0] == p && grid[1][0] == p && grid[2][0] == p) return true;
-    	if (grid[0][1] == p && grid[1][1] == p && grid[2][1] == p) return true;
-    	if (grid[0][2] == p && grid[1][2] == p && grid[2][2] == p) return true;
-    	if (grid[0][0] == p && grid[1][1] == p && grid[2][2] == p) return true;
-    	if (grid[2][0] == p && grid[1][1] == p && grid[0][2] == p) return true;
-    	return false;
+        if (grid[0][0] == p && grid[0][1] == p && grid[0][2] == p) return true;
+        if (grid[1][0] == p && grid[1][1] == p && grid[1][2] == p) return true;
+        if (grid[2][0] == p && grid[2][1] == p && grid[2][2] == p) return true;
+        if (grid[0][0] == p && grid[1][0] == p && grid[2][0] == p) return true;
+        if (grid[0][1] == p && grid[1][1] == p && grid[2][1] == p) return true;
+        if (grid[0][2] == p && grid[1][2] == p && grid[2][2] == p) return true;
+        if (grid[0][0] == p && grid[1][1] == p && grid[2][2] == p) return true;
+        if (grid[2][0] == p && grid[1][1] == p && grid[0][2] == p) return true;
+        return false;
     }
     
     public char getCurrentPlayer() {
-    	return currentPlayer;
+        return currentPlayer;
     }
     
     private void switchPlayer() {
-    	if (currentPlayer == 'X') {
-    		currentPlayer = O;
-    	} else {
-    		currentPlayer = X;
-    	}
+        if (currentPlayer == 'X') {
+            currentPlayer = O;
+        } else {
+            currentPlayer = X;
+        }
     }
     
     public boolean didXWin() {
-    	return didPlayerWin(X);
+        return didPlayerWin(X);
     }
-
+    
     public boolean didOWin() {
-    	return didPlayerWin(O);
+        return didPlayerWin(O);
     }
-
+    
     public boolean isDraw() {
         return isBoardFull() && !didXWin() && !didOWin();
     }
     
     public boolean isGameOver() {
-    	return didXWin() || didOWin() || isDraw();
+        return didXWin() || didOWin() || isDraw();
     }
-
+    
     public boolean isBoardFull() {
-    	if (grid[0][0] == EMPTY) return false;
-    	if (grid[0][1] == EMPTY) return false;
-    	if (grid[0][2] == EMPTY) return false;
-    	if (grid[1][0] == EMPTY) return false;
-    	if (grid[1][1] == EMPTY) return false;
-    	if (grid[1][2] == EMPTY) return false;
-    	if (grid[2][0] == EMPTY) return false;
-    	if (grid[2][1] == EMPTY) return false;
-    	if (grid[2][2] == EMPTY) return false;
-    	return true;
+        if (grid[0][0] == EMPTY) return false;
+        if (grid[0][1] == EMPTY) return false;
+        if (grid[0][2] == EMPTY) return false;
+        if (grid[1][0] == EMPTY) return false;
+        if (grid[1][1] == EMPTY) return false;
+        if (grid[1][2] == EMPTY) return false;
+        if (grid[2][0] == EMPTY) return false;
+        if (grid[2][1] == EMPTY) return false;
+        if (grid[2][2] == EMPTY) return false;
+        return true;
     }
-
+    
     public boolean putMark(int r, int c) {
         if (grid[r][c] == EMPTY) {
             grid[r][c] = currentPlayer;
